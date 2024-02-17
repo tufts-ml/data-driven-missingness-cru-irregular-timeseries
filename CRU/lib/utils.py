ff73eea2879a60afa1de6a8867680f933fee6005
Preetish Rath <prath01@cc1gpu002.pax.tufts.edu>
2024-02-17 16:40:19 -0500

    Cleaning up LICENSE file

--- CRU/lib/utils.py
+++ CRU/lib/utils.py
@@ -1,27 +1,4 @@
-# Modeling Irregular Time Series with Continuous Recurrent Units (CRUs)
-# Copyright (c) 2022 Robert Bosch GmbH
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as published
-# by the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Affero General Public License for more details.
-#
-# You should have received a copy of the GNU Affero General Public License
-# along with this program.  If not, see <https://www.gnu.org/licenses/>.
-#
-# This source code is derived from Pytorch RKN Implementation (https://github.com/ALRhub/rkn_share)
-# Copyright (c) 2021 Philipp Becker (Autonomous Learning Robots Lab @ KIT)
-# licensed under MIT License
-# cf. 3rd-party-licenses.txt file in the root directory of this source tree.
 
-# This source code is derived from Latent ODEs for Irregularly-Sampled Time Series (https://github.com/YuliaRubanova/latent_ode)
-# Copyright (c) 2019 Yulia Rubanova
-# licensed under MIT License
-# cf. 3rd-party-licenses.txt file in the root directory of this source tree.
 
 import torch
 from torch import nn
